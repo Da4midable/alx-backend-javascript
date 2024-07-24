886cda407469b2abd2df3d905f4f8b0664a2e4e4
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-07-24 23:11:12 +0000

    Let's Reject 'em All

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((resolve, reject) => {
-    resolve();
-    reject(new Error(`${fileName} cannot be processed`));
-  });
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
