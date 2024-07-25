1013dde8a9b0195ee9a8e26e7a07f6d722dcd752
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-07-25 00:06:36 +0000

    Another Multiple Promises Handling +

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
+const handleProfileSignup = (firstName, lastName, fileName) => {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ])
+  .then(results => {
+    return results.map(result => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
-}
+      value: result.value || result.reason
+    }));
+  });
+};
+
+export default handleProfileSignup;
