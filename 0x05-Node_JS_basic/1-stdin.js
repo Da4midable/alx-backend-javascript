a47a07294df924f4d48d3fd478627a93834a7178
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-18 15:35:23 +0100

    Task 3

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,14 @@
 // 1-stdin.js
 
-const question = 'Welcome to Holberton School, what is your name?\n';
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-function askName() {
-    process.stdout.write(question);
-}
-
-process.stdin.on('data', function(data) {
-    const answer = data.toString().trim();
-    process.stdout.write(`Your name is: ${answer}\n`);
-    process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-process.on('exit', function() {
-    if (!process.stdin.isTTY) {
-        process.stdout.write('This important software is now closing\n');
-    }
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-askName();
