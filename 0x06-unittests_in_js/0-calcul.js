14cc84d44741f70bbd32cb1521183eeb359eb587
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-20 15:47:47 +0100

    unit test - Task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,6 @@
-export default function calculateNumber(a, b) {
-		return a + b;
-	}
+// 0-calcul.js
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
